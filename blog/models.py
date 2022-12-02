ca3c5fe1ddbba697208f976fc3ba9d014009ba7b
alexeyglinkin <aleksey.glinkin@skillbox.ru>
2022-12-02 08:20:18 +0300

    Add post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    text = models.TextField()
+    post_image = models.ImageField(upload_to="event_images")
+    post_date = models.DateField()
+
+
 # Create your models here.
--- testblog/settings.py
+++ testblog/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
+    'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'testblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testblogdb',
+        'USER': 'postgres',
+        'PASSWORD': "Glin33A4",
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -121,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
